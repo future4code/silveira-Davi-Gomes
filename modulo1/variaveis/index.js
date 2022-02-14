c27d595b5f7109c881e1529594f7c284eba12d05
araujod08 <davia9a@gmail.com>
2022-02-14 16:27:58 -0300

    variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,42 @@
+
+/* resposta 1.
+  10
+  10 5*/
+ 
+/*resposta 2.
+10 10 10*/
+
+/*let horaTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+let ganhosPorHora = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${ganhosPorHora/horaTrabalhadas} por hora`)*/
+
+let nome = prompt ("Digitar seu nome ");
+let idade = prompt ("Digitar sua idade"); 
+idade = Number(idade);
+
+console.log(typeof nome);
+console.log(typeof idade);
+/**ele indentificou que "nome" e uma string e "idade" e um number */
+
+console.log("ola", nome, "você tem", idade);
+
+let corDaCamisa = prompt ("você está usando uma camisa azul?");
+let oculos = prompt ("você está usando oculos?");
+let beberAgua = prompt ("você já bebeu água hoje?");
+
+console.log(corDaCamisa);
+console.log(oculos);
+console.log(beberAgua);
+
+let a = 10 
+let b = 20
+let c = 30
+
+c = b 
+b = c
+c = a
+  
+
+console.log("o valor de a e", a);
+console.log("o valor de b e", b);
+console.log("o valor de c e", c);
