9e05b25cdf7ba4be2f75265c411fa38b9c31e2a0
Araujod12 <davia9a@gmail.com>
2022-04-29 13:31:34 -0300

    formulario

--- modulo3/projeto-labex/projeto-labex/src/pages/CreateTripPage.js
+++ modulo3/projeto-labex/projeto-labex/src/pages/CreateTripPage.js
@@ -1,99 +1,117 @@
-// import React, { useState, useEffect }from 'react'
-// import styled from 'styled-components'
-// import { useNavigate } from 'react-router-dom'
-// import axios from 'axios'
-// import useProtectedPage from '../Hooks/useProtectedPage'
-
-// const GlobalStyles = styled.div`
-//     padding: 0;
-//     margin: 0;
-//     box-sizing: border-box;
-//     text-decoration: none;
-//     display: flex;
-//     height: 100vh;
-//     flex-direction: column;  
-//     color: rgb(217, 176, 255);
-// `
+import React from 'react'
+import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import useProtectedPage from '../Hooks/useProtectedPage'
+import { Planets } from '../constants/planets'
+import useForm from '../Hooks/useForm'
 
 
-// export default function CreateTripPage() {
-//     useProtectedPage()
-//     const navigate = useNavigate()
+const GlobalStyles = styled.div`
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+    display: flex;
+    height: 100vh;
+    flex-direction: column;  
+    color: rgb(217, 176, 255);
+`
 
-//     const goHomePage = () => {
-//         navigate("/")
-//     }
-//     const [name, setname] = useState("")
-//     const [planet, setPlanet] = useState("")
-//     const [date, setDate] = useState("")
-//     const [description, setDescription] = useState("")
-//     const [durationInDays, setDurationInDays] = useState("")
 
-//     const token = localStorage.getItem("token")
-//     axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labeX/davi-gomes-silveira/trips")
-//         .then((res) => {
-//             alert("viagem criada!!!")
+export default function CreateTripPage() {
+    useProtectedPage()
+    const navigate = useNavigate()
 
-//         }).catch((err) => {
-//             alert("ocorreu um erro!!!")
+    const goHomePage = () => {
+        navigate("/")
+    }
+    const { form, onChange, cleanFields } = useForm({
+        name: "",
+        planet: "",
+        date: "",
+        description: "",
+        durationInDays: "",
+    })
+    const create = (event) => {
+        event.preventDefault()
+        createTrip()
+        cleanFields()
+    }
 
-//         })
-//     const onChangeName = (event) => {
-//         setname(event.target.value)
+    const createTrip = () => {
+        const headers = {
+            headers: {
+                "content-type": "application/json",
+                "auth": localStorage.getItem("token")
 
-//     }
+            }
+        }
 
-//     const onChangePlanet = (event) => {
-//         setPlanet(event.target.value)
-//     }
 
-//     const onChangeDate = (event) => {
-//         setDate(event.target.value)
-//     }
-//     const onChanDescription = (event) => {
-//         setDescription(event.target.value)
-//     }
-//     const onChanDurationInDays = (event) => {
-//         setDurationInDays(event.target.value)
-//     }
+        axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labeX/davi-gomes-silveira/trips", form, headers)
+            .then((res) => {
+                alert("viagem criada!!!")
 
-//     return (
-//         <GlobalStyles>
-//             <div>
-//                 <button onClick={goHomePage}>
-//                     Home
-//                 </button>
-//                 <h1>Sala de comando</h1>
-//                 <div>
-//                     <input
-//                         placeholder="Nome"
-//                         type="name"
-//                         value={name}
-//                         onChange={onChangeName}
-//                     />
-//                     <select>
-//                         <option>Escolha um planeta</option>
-//                     </select>
-//                     <input
-//                         placeholder="Data"
-//                         type="date"
-//                         value={date}
-//                         onChange={onChangeDate}
-//                     />
-//                     <input
-//                         placeholder="Descrição"
-//                         type="date"
-//                         value={description}
-//                         onChange={onChanDescription}
-//                     />
-//                     <input
-//                         placeholder="Duração em dias"
-//                         value={durationInDays}
-//                         onChange={onChanDurationInDays}
-//                     />
-//                 </div>
-//                 <button>Criar</button>
-//             </div>
-//         </GlobalStyles >
-//     )
-// }
+            }).catch((err) => {
+                alert("ocorreu um erro!!!")
+
+            })
+        
+     }   
+            const getPlanet = Planets.map((planets) => {
+            return <option key={planets} value={planets}>{planets}</option>
+        })
+    
+    return (
+        <GlobalStyles>
+            <form onSubmit={create}>
+
+                <button onClick={goHomePage}>
+                    Home
+                </button>
+                <h1>Sala de comando</h1>
+                <div>
+                    <input
+                        name={"name"}
+                        placeholder="Nome"
+                        type="name"
+                        value={form.name}
+                        onChange={onChange}
+                    />
+
+                    <select
+                        name={"planet"}
+                        value={form.planet}
+                        onChange={onChange}
+                        placeholder="Planet"
+                    >
+                        <option value="" disabled>Escolha um planeta</option>
+                        {getPlanet}
+                    </select>
+                    <input
+                        name={"date"}
+                        placeholder="Data"
+                        type="date"
+                        value={form.date}
+                        onChange={onChange}
+                    />
+                    <input
+                        name={"description"}
+                        placeholder="Descrição"
+                        type="date"
+                        value={form.description}
+                        onChange={onChange}
+                    />
+                    <input
+                        name={"durationInDays"}
+                        placeholder="Duração em dias"
+                        value={form.durationInDays}
+                        onChange={onChange}
+                    />
+                </div>
+                <button>Criar</button>
+            </form>
+        </GlobalStyles >
+    )
+}
