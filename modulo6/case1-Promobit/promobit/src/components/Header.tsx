9cc05f4ba84cb37f756e505ba55734ca8b621e3c
Davi de araujo gomes <davia9a@gmail.com>
2022-07-26 00:18:17 -0300

    estilezaçao header

--- modulo6/case1-Promobit/promobit/src/components/Header.tsx
+++ modulo6/case1-Promobit/promobit/src/components/Header.tsx
@@ -1,33 +1,15 @@
-import styled from "styled-components"
 import { NavigateFunction } from "react-router-dom"
 import React from "react"
+import {HeaderContainer} from "./style"
 
-// const HeaderContainer = styled.div`
-
-// width: 100%;
-// height: 56px;
-// left: 0px;
-// top: 0px;
-// background: #5C16C5;
-
-// h2{
-//     font-size: 1.5em;
-//     margin-block-start: 10px;
-//     margin-block-end: 10px;
-//     margin-inline-start:50px;
-//     margin-inline-end: 0px;
-//     font-weight: bold;
-//     color: #FFFFFF ;
-// }
-// `
 
 interface Props {
     navigate: NavigateFunction
 }
 export function Header() {
     return (
-        <div>
+        <HeaderContainer>
             <h2>TMDB</h2>
-        </div>
+        </HeaderContainer>
     )
 }
